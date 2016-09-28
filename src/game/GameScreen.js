b5361ef0c8b95354f834f7249120669eb60876a8
Maksimiljan Spogis <maksimiljan.spogis@miceanddice.com>
2016-09-28 12:25:12 +0900

    findClusters() - finds existing clusters

--- src/game/GameScreen.js
+++ src/game/GameScreen.js
@@ -8,177 +8,140 @@ import src.game.actors.Gem as Gem;
 
 var headerImg = new Image({url: "resources/images/ui/header.png"});
 
-exports = Class(ui.View, function (supr) {
-
-    this.init = function (opts) {
-
+exports = Class(ui.View, function(supr) {
+    this.init = function(opts) {
         opts = merge(opts, {
-            x:0,
-            y:0,
+            x: 0,
+            y: 0,
             width: 576,
             height: 1024
         });
-
         supr(this, 'init', [opts]);
         this.build();
     };
-
-    this.build = function () {
-
-
-        
-
-      var background = new ui.ImageView({
-        superview: this,
-        x:0,
-        y:0,
-        width: 576,
-        height: 1024,
-        image: "resources/images/ui/background.png"
-      });
-
-
-      // Setup header
-      var header = new ui.ImageView({
-        superview: this,
-        canHandleEvents: true,
-        x: 576 / 2 - 249 / 2,
-        y: 0,
-        width: 249,
-        height:166,
-        image: headerImg
-      });
-      this.countdown = new ui.TextView({
-			superview: this,
-			visible: true,
+    this.build = function() {
+        var background = new ui.ImageView({
+            superview: this,
+            x: 0,
+            y: 0,
+            width: 576,
+            height: 1024,
+            image: "resources/images/ui/background.png"
+        });
+        // Setup header
+        var header = new ui.ImageView({
+            superview: this,
+            canHandleEvents: true,
+            x: 576 / 2 - 249 / 2,
+            y: 0,
+            width: 249,
+            height: 166,
+            image: headerImg
+        });
+        this.countdown = new ui.TextView({
+            superview: this,
+            visible: true,
             x: 576 / 2 - 249 / 2,
             y: 70,
             width: 249,
-            height:80,
-			size: 32,
-			color: '#FFFFFF',
-			opacity: 0.7,
+            height: 80,
+            size: 32,
+            color: '#FFFFFF',
+            opacity: 0.7,
             verticalAlign: 'middle',
-		    textAlign: 'center',
-		    multiline: false
-	  });
-
-      this.countdown.setText('0');
+            textAlign: 'center',
+            multiline: false
+        });
+        this.countdown.setText('0');
         // Put the gems to view
-		var xOffset = 22;
-		var yOffset = 314;
+        var xOffset = 22;
+        var yOffset = 314;
         var xPadding = 1;
-		var yPadding = 1;
+        var yPadding = 1;
         this.layout = [
-                    [1, 1, 1, 1, 1, 1, 1, 1],
-                    [1, 1, 1, 1, 1, 1, 1, 1],
-                    [1, 1, 1, 1, 1, 1, 1, 1],
-                    [1, 1, 1, 1, 1, 1, 1, 1],
-                    [1, 1, 1, 1, 1, 1, 1, 1],
-                    [1, 1, 1, 1, 1, 1, 1, 1],
-                    [1, 1, 1, 1, 1, 1, 1, 1],
-                    [1, 1, 1, 1, 1, 1, 1, 1]
-                    ];
-
-		this.gemsOnScreen = [];
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1]
+        ];
+        this.gemsOnScreen = [];
         this.inactive = true;
-        var clusters =[];
-
-		for (var row = 0, len = this.layout.length; row < len; row++) {
+        this.clusters = [];
+        for (var row = 0, len = this.layout.length; row < len; row++) {
             var gemRow = [];
-			for (var col = 0; col < len; col++) {
-				
+            for (var col = 0; col < len; col++) {
                 if (this.layout[row][col] !== 0) {
-					var gem = new Gem();
+                    var gem = new Gem();
                     gem.style.x = xOffset + col * (gem.style.width + xPadding);
-					gem.style.y = yOffset + row * (gem.style.height + yPadding);
-					this.addSubview(gem);
-					gemRow.push(gem);
-
-				} else {
+                    gem.style.y = yOffset + row * (gem.style.height + yPadding);
+                    this.addSubview(gem);
+                    gemRow.push(gem);
+                } else {
                     gemRow.push(null);
                 }
-                
-			}
+            }
             this.gemsOnScreen.push(gemRow)
-		}
-
-
-        findClusters.call(this)
-	};
+        }
+    };
 });
 
+function findClusters() {
 
-        function findClusters() {
+    clusters = this.clusters;
 
-                clusters = []
-        
-                for (var row = 0; row < this.gemsOnScreen.length; row++) {
-                    var matchLength = 1;
-                    var matchHeight = 1;
-
-                    for (var col = 0; col < this.gemsOnScreen[row].length; col++) {
-                        var checkLength = false;
-                        var checkHeight = false;
-                        
-                        if( this.layout[row][col] !== 0 && 
-                        row <= this.gemsOnScreen[row].length - 1 &&
-                        this.gemsOnScreen[row][col + 1] &&
-                        this.gemsOnScreen[row][col].gemType == this.gemsOnScreen[row][col + 1].gemType) {
-                                matchLength += 1;
-                        } else {
-                            checkLength = true;
-                        }
-                        
-
-                        if (checkLength) {
-                            if (matchLength >= 3) {
-                                clusters.push({  row: row, column: col - (matchLength - 1),
-                                             length: matchLength, horizontal: true });
-                            }
-                            matchLength = 1;
-                            
-                        }
-
-                    }                
-
+    for (var row = 0; row < this.gemsOnScreen.length; row++) {
+        var matchLength = 1;
+        var matchHeight = 1;
+        for (var col = 0; col < this.gemsOnScreen[row].length; col++) {
+            var checkLength = false;
+            var checkHeight = false;
+            if (this.layout[row][col] !== 0 && row <= this.gemsOnScreen[row].length - 1 && this.gemsOnScreen[row][col + 1] && this.gemsOnScreen[row][col].gemType == this.gemsOnScreen[row][col + 1].gemType) {
+                matchLength += 1;
+            } else {
+                checkLength = true;
+            }
+            if (checkLength) {
+                if (matchLength >= 3) {
+                    clusters.push({
+                        row: row,
+                        column: col - (matchLength - 1),
+                        length: matchLength,
+                        horizontal: true
+                    });
                 }
-
-                for (var row = 0; row < this.gemsOnScreen.length; row++) {
-                    var matchLength = 1;
-                    var matchHeight = 1;
-
-                    for (var col = 0; col < this.gemsOnScreen[row].length; col++) {
-                        var checkLength = false;
-                        var checkHeight = false;
-                        
-                        if( this.layout[row][col] !== 0 && //follow the level layout
-                        row <= this.gemsOnScreen.length - 1 &&
-                        this.gemsOnScreen[row + 1] &&
-                        this.gemsOnScreen[row][col].gemType == this.gemsOnScreen[row + 1][col].gemType) {
-                                matchHeight += 1;
-                                console.log(row, col)
-                                console.log(row + 1, col)
-                        } else {
-                            checkHeight = true;
-                        }
-                        
-
-                        if (checkHeight) {
-                            console.log("checkHeightttt")
-
-                            if (matchHeight >= 3) {
-                                clusters.push({  row: row, column: col - (matchHeight - 1),
-                                                length: matchHeight , horizontal: false });
-                            }
-                            matchHeight = 1;
-                            
-                        }
-
-                    }                
-
-
-                }
-                   
-        console.log(clusters)
+                matchLength = 1;
+            }
         }
+    }
+
+    for (var col = 0; col < this.gemsOnScreen.length; col++) {
+        var matchLength = 1;
+        var matchHeight = 1;
+        for (var row = 0; row < this.gemsOnScreen[col].length; row++) {
+            var checkLength = false;
+            var checkHeight = false;
+            console.log(row, col)
+            if (this.layout[row][col] !== 0 && //follow the level layout
+                row <= this.gemsOnScreen.length - 1 && this.gemsOnScreen[row + 1] && this.gemsOnScreen[row][col].gemType == this.gemsOnScreen[row + 1][col].gemType) {
+                matchHeight += 1;
+            } else {
+                checkHeight = true;
+            }
+            if (checkHeight) {
+                if (matchHeight >= 3) {
+                    clusters.push({
+                        row: row - (matchHeight - 1),
+                        column: col,
+                        length: matchHeight,
+                        horizontal: false
+                    });
+                }
+                matchHeight = 1;
+            }
+        }
+    }
+}
